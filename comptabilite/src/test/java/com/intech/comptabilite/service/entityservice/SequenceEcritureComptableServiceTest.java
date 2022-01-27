d08d62769ed553d33e2ee39caf58a3e9bd04e309
Lucas <lucas@squadeasy.com>
2022-01-27 16:43:52 +0100

    Add mock test on SequenceEcritureComptable

--- comptabilite/src/test/java/com/intech/comptabilite/service/entityservice/SequenceEcritureComptableServiceTest.java
+++ comptabilite/src/test/java/com/intech/comptabilite/service/entityservice/SequenceEcritureComptableServiceTest.java
@@ -1,7 +1,32 @@
 package com.intech.comptabilite.service.entityservice;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.intech.comptabilite.model.SequenceEcritureComptable;
+import com.intech.comptabilite.model.SequenceId;
+import com.intech.comptabilite.repositories.SequenceEcritureComptableRepository;
+import com.intech.comptabilite.service.exceptions.NotFoundException;
+
+
 public class SequenceEcritureComptableServiceTest {
 	
 	private SequenceEcritureComptableService sequenceEcritureComptableService = new SequenceEcritureComptableService();
+	
+	@MockBean
+	private SequenceEcritureComptableRepository repository;
+	
+	@Test
+	public void getDernierValeurByCodeAndAnnee() throws NotFoundException {
+		String journalCode = "AC";
+		Integer annee = 2022;
+		Mockito.when(repository.findById(new SequenceId(journalCode, annee))).thenReturn(Optional.of(new SequenceEcritureComptable("AC", 2022, 1)));
+		Assertions.assertEquals(1, sequenceEcritureComptableService.getDernierValeurByCodeAndAnnee(journalCode, annee));
+	}
 
 }
