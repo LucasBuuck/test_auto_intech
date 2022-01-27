1360d2cdccdfa3f0f7fac633bbed34b58178b82e
Lucas <lucas@squadeasy.com>
2022-01-27 12:38:16 +0100

    JournalComptableTest

--- comptabilite/src/test/java/com/intech/comptabilite/service/entityservice/JournalComptableServiceTest.java
+++ comptabilite/src/test/java/com/intech/comptabilite/service/entityservice/JournalComptableServiceTest.java
@@ -1,5 +1,32 @@
 package com.intech.comptabilite.service.entityservice;
 
-public class JournalComptableServiceTest {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.intech.comptabilite.model.JournalComptable;
+
+public class JournalComptableServiceTest {
+	
+	private JournalComptableService journalComptableService = new JournalComptableService();
+
+	@Test
+	 public void getByCode() {
+		List<JournalComptable> pList = new ArrayList<>();
+		pList.add(new JournalComptable("j1", "one"));
+		pList.add(new JournalComptable("j2", "two"));
+		pList.add(new JournalComptable("j3", "three"));
+		pList.add(new JournalComptable("j4", "four"));		
+		
+		String foundJournalCode = "j3";
+		String notFoundJournalCode = "j5";
+		
+		JournalComptable foundJournal = journalComptableService.getByCode(pList, foundJournalCode);
+		JournalComptable notfoundJournal = journalComptableService.getByCode(pList, notFoundJournalCode);
+		
+        Assertions.assertEquals("three", foundJournal.getLibelle());
+        Assertions.assertNull(notfoundJournal);
+	    }
 }
